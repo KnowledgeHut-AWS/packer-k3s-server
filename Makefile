f2a3d8bc9bc74ea37aaad8142d69e71922e18006
Bryan Dollery <bryan.dollery@gmail.com>
2020-08-04 15:37:06 +0300

    working now

--- Makefile
+++ Makefile
@@ -8,16 +8,18 @@ start:
 		--env AWS_SECRET_ACCESS_KEY="$$(sed -n 3p creds/credentials | sed 's/.*=//')" \
 		--env OWNER=$$OWNER \
 		-v /var/run/docker.sock:/var/run/docker.sock \
-		-v $$(pwd):/work \
-		-w /work \
-		--name pawst_ps \
+		-v k3s_token:/token \
+		-v $$PWD:/$$(basename $$PWD) \
+		-w /$$(basename $$PWD) \
+		--name $$(basename $$PWD) \
+		--hostname $$(basename $$PWD) \
 		bryandollery/terraform-packer-aws-alpine
 
 stop:
-	docker rm -f pawst_ps 2> /dev/null || true
+	docker rm -f $$(basename $$PWD) 2> /dev/null || true
 
 exec:
-	docker exec -it pawst_ps bash || true
+	docker exec -it $$(basename $$PWD) bash || true
 
 build:
 	packer build packer.json
--- provision.sh
+++ provision.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
-sudo apt update -y && sudo apt install -y curl vim jq git make
-curl -Ls get.docker.com | sh
+sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
-sudo docker pull nginx
 curl -sfL https://get.k3s.io | sh -
-mkdir -p /var/lib/rancher/k3s/server
-mkdir -p /home/ubuntu/token
-sudo touch /etc/systemd/system/tokenServer.service
-sudo chmod a+rw /etc/systemd/system/tokenServer.service
+sudo systemctl enable k3s
+sudo systemctl start k3s
+sleep 10
+sudo cat /var/lib/rancher/k3s/server/token
 
-cat << EOF >> /etc/systemd/system/tokenServer.service
-[Unit]
-Description=K3S Token Server on port 8765
-After=k3s.target
-
-[Service]
-ExecStartPre=cp /var/lib/rancher/k3s/server/token /home/ubuntu/token/index.html
-ExecStartPre=chown ubuntu:ubuntu /home/ubuntu/token/index.html
-ExecStartPre=chmod a+rw /home/ubuntu/token/index.html
-ExecStart=docker run --name token-server -p 8765:80 --restart=always -v /home/ubuntu/token:/usr/share/nginx/html:ro -d nginx
-ExecStop=docker rm -f token-server
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo chmod a-w /etc/systemd/system/tokenServer.service
-sudo systemctl enable tokenServer
-sudo systemctl start tokenServer
+sudo cp /var/lib/rancher/k3s/server/token /home/ubuntu/token
+sudo chmod a+rw /home/ubuntu/token
+sudo chown ubuntu:ubuntu /home/ubuntu/token
